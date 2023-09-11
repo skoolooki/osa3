23a363bbfc462f93c3ea34b61ef9492839652b21
Slava <shukov.sl@gmail.com>
2023-09-11 14:15:19 +0300

    nevermind

--- backindex.js
+++ backindex.js
@@ -71,7 +71,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.PORT
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- modules/mongoose.js
+++ modules/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
 
-const url = process.MONGODB_URI
+const url = process.env.MONGODB_URI
 
 
 console.log('connecting to', url)
