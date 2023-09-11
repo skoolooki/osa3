55debd6cce9c1bc88f4cd792190a8e0f56db99ef
Slava <shukov.sl@gmail.com>
2023-09-11 14:13:42 +0300

    test

--- backindex.js
+++ backindex.js
@@ -71,7 +71,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const PORT = process.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
